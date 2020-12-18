b527fb48bcf11f2c3c57c970359cab34b8068830
sarahayu <46186848+sarahayu@users.noreply.github.com>
2020-12-17 20:06:02 -0800

    Update index.js
    removed debug comments

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const Discord = require('discord.js')
 const fetch = require('node-fetch')
-const decode = require('unescape')
 const TurndownService = require('turndown')
-const commonmark = require('commonmark')
-const client = new Discord.Client()
 
 const responses = require('./responses.json')
 const { token, key, id } = require('./secure.json')
+const client = new Discord.Client()
 const turndown  = new TurndownService()
 
 function log(message) 
@@ -55,7 +53,7 @@ client.on('message', message => {
           throw Error('Too many requests!')
         })
         .then(data => {
-          console.log(data)
+          
           if (data.searchInformation.totalResults == 0 || !data.items[0].pagemap.answer || data.items[0].pagemap.answer.length == 0)
           {
             channel.send(`Uh oh, there were no answers on StackOverflow for \`${question}\`! Try another query`)
@@ -74,49 +72,27 @@ client.on('message', message => {
           })
           .then(data => {
             const answer = data.items[0]
-            // console.log(answer.owner.display_name)
-            // console.log(answer.link)
-            // console.log(answer.is_accepted)
-            // console.log(decode(answer.body))
-            // console.log(answer.score)
-            // channel.send(decode(answer.body))
-            const reader = new commonmark.Parser()
-            const writer = new commonmark.HtmlRenderer()
 
             let answerStr = turndown.turndown(unescape(answer.body)).replace(/>/g, '\\>')
             if (answerStr.length > 1024) answerStr = answerStr.substring(0, 1021) + '...'
 
-            channel.send({embed: {
-              color: 0xff8e42,
-              author: {
-                name: answer.owner.display_name
-              },
-              title: answer.title,
-              url: answer.link,
-              description: answerStr
-            }
-          });
-
-            // // const reader = new commonmark.Parser()
-            // // const writer = new commonmark.HtmlRenderer()
-            // // console.log(decode(answer.body))
-            // console.log(turndown.turndown(unescape(answer.body)).replace(/>/g, '\\>'))
+            channel.send({
+              embed: {
+                color: 0xff8e42,
+                author: {
+                  name: answer.owner.display_name
+                },
+                title: answer.title,
+                url: answer.link,
+                description: answerStr
+              }
+            })
+          })
+          .catch(err => {
+            console.log(err)
           })
 
         })
-      // request.get({
-      //   url: `https://www.googleapis.com/customsearch/v1?key=${key}&num=1&cx=${id}&q=${escape(question)}`,
-      //   json: true
-      // }, (err, res, data) => {
-      //   if (!err && res.statusCode === 200)
-      //   {
-      //     console.log(data)
-      //     if (data.searchInformation.totalResults == 0)
-      //       channel.send(`Uh oh, there were no answers on StackOverflow for \`${question}\`! Try another query`)
-      //     else
-      //       console.log(data.items[0].pagemap.answer[0].text)
-      //   }
-      // })
     }
   }
   else if (messageStr.startsWith('?') || message.mentions.has(client.user))
